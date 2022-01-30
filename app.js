6b2e908701efa696af6b5cb4930634a2d71fc483
goju2k <goju2k@naver.com>
2022-01-30 18:34:13 +0900

    added cors modules

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
+var cors = require('cors')
+
+app.use(cors());
+
 //test api
 app.get('/category/list', (req, res) => {
     console.log('[req]['+new Date()+'] /category/list')
